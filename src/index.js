bb47373f53562e0f1b2bd31cf31c04f2969ee24b
Tynan DeBold <thdebold@gmail.com>
2020-03-10 17:27:56 +0100

    add carousels

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 
   function init() {
     handleEvents();
+    initCarousels();
   }
 
   function handleEvents() {
@@ -15,5 +16,31 @@
       .addEventListener("click", function() {
         document.documentElement.scrollTop = 0;
       });
+  }
+
+  function initCarousels() {
+    var $carouselWrappers = document.querySelectorAll(".carousel-wrapper");
+
+    for (var i = 0; i < $carouselWrappers.length; i++) {
+      var $wrapper = $carouselWrappers[i];
+      initCarouselWrapper($wrapper);
+    }
+  }
+
+  function initCarouselWrapper(wrapper) {
+    var $carousel = wrapper.querySelector(".carousel");
+    $carousel.classList.remove("is-hidden");
+    $carousel.offsetHeight;
+    var flkty = new Flickity($carousel, { pageDots: false });
+    var carouselStatus = wrapper.querySelector(".carousel-info");
+
+    function updateStatus() {
+      var slideNumber = flkty.selectedIndex + 1;
+      carouselStatus.textContent = slideNumber + " of " + flkty.slides.length;
+    }
+
+    updateStatus();
+
+    flkty.on("select", updateStatus);
   }
 })();
