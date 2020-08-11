3202194cf85ff06e394b21020e81eb164cac3e53
Tynan DeBold <thdebold@gmail.com>
2020-08-11 11:37:52 +0200

    refactor some js; update status text color

--- src/index.js
+++ src/index.js
@@ -98,46 +98,49 @@
     );
   }
 
-  var c = document.querySelector(".canvas");
-  var $ = c.getContext("2d");
+  var canvas = document.querySelector(".canvas");
+  var canvasContext = canvas.getContext("2d");
+  var colorConstant = 161;
 
-  var col = function (x, y, r, g, b) {
-    $.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-    $.fillRect(x, y, 1, 1);
-  };
-  var R = function (x, y, t) {
-    return Math.floor(155 + 64 * Math.cos((x * x - y * y) / 300 + t));
-  };
+  function col(x, y, r, g, b) {
+    canvasContext.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+    canvasContext.fillRect(x, y, 1, 1);
+  }
 
-  var G = function (x, y, t) {
+  function R(x, y, t) {
+    return Math.floor(colorConstant + 64 * Math.cos((x * x - y * y) / 300 + t));
+  }
+
+  function G(x, y, t) {
     return Math.floor(
-      155 +
+      colorConstant +
         64 * Math.sin((x * x * Math.cos(t / 4) + y * y * Math.sin(t / 3)) / 300)
     );
-  };
+  }
 
-  var B = function (x, y, t) {
+  function B(x, y, t) {
     return Math.floor(
-      155 +
+      colorConstant +
         64 *
           Math.sin(
             5 * Math.sin(t / 9) +
               ((x - 100) * (x - 100) + (y - 100) * (y - 100)) / 1100
           )
     );
-  };
+  }
 
   var t = 0;
 
-  var run = function () {
+  function run() {
     for (x = 0; x <= 35; x++) {
       for (y = 0; y <= 35; y++) {
         col(x, y, R(x, y, t), G(x, y, t), B(x, y, t));
       }
     }
     t = t + 0.015;
+
     window.requestAnimationFrame(run);
-  };
+  }
 
   run();
 })();
